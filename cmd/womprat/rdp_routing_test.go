2ff9088bf9d4f0115e71383f0723c80a99c6ed42
Rui Carmo <rui.carmo@gmail.com>
2026-06-10 20:41:58 +0100

    Guard RDP tsnet dial routing

---
+++ cmd/womprat/rdp_routing_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestRDPHandlerUsesInjectedDialerOnly(t *testing.T) {
+	src, err := os.ReadFile("rdp.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	text := string(src)
+	if !strings.Contains(text, "rdp.NewClientWithDialContext") {
+		t.Fatal("RDP handler must use injected dialer")
+	}
+	if strings.Contains(text, "rdp.NewClient(") {
+		t.Fatal("RDP handler must not use direct rdp.NewClient")
+	}
+	if !strings.Contains(text, "return ts.Dial(ctx, network, addr)") {
+		t.Fatal("RDP handler must dial through tsnet")
+	}
+}
